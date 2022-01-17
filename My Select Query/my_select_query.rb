15a47475182d3eb4aff39d1a0f04d6a3e4b0cbc0
Dastan Ramazan <50314411+dastanramazan@users.noreply.github.com>
2022-01-17 20:19:09 +0600

    Add files via upload

--- My Select Query/my_select_query.rb
+++ My Select Query/my_select_query.rb
@@ -1,1 +1,26 @@
+require 'csv'
 
+class MySelectQuery
+    @csv_string = []  # @ - field - привязка иммено к классу, чтобы потом использовать
+    def initialize(csv) # все конструкторы начинаюся с initialize
+        @csv_string = csv #CSV comma separated values
+    end
+
+    def where(column_name, criteria)
+        new_array = Array.new
+        lines = @csv_string.split("\n")
+        headers = lines[0].split(',')
+
+        column_name_index = headers.index(column_name)
+        for line in lines do # do необязательно использовать
+            a = line.parse_csv[column_name_index]
+
+            if (a == criteria)
+                new_array.push(line)
+            end
+        end
+        return new_array
+    end
+end
+
+#создать класс и исполнить один метод where с двумя параметрами. Возвращает critearia с именем column_name
