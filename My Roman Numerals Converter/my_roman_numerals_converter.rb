cfdf8523f1fbbd68b9dd43521b14fddfcd17f44e
Dastan Ramazan <50314411+dastanramazan@users.noreply.github.com>
2022-01-17 19:54:15 +0600

    Add files via upload

--- My Roman Numerals Converter/my_roman_numerals_converter.rb
+++ My Roman Numerals Converter/my_roman_numerals_converter.rb
@@ -1,1 +1,17 @@
+ROMAN_NUMS = {
+  "M" => 1000,
+  "CM" => 900, "D" => 500, "CD" => 400, "C" => 100,
+  "XC" => 90,  "L" => 50,  "XL" => 40,  "X" => 10,
+  "IX" => 9,   "V" => 5,   "IV" => 4,   "I" => 1
+}
 
+def my_roman_numerals_converter(number)
+    answer = ""
+    ROMAN_NUMS.map do |letter, value| 
+        amount, number = number.divmod(value)
+        answer << letter * amount
+    end
+    return answer
+end
+
+#puts(roman(10))
